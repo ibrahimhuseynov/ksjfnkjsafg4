208e33aedc916cd7f4e08279236f737b1f8a19e4
ibrahimhuseynov <99806021+ibrahimhuseynov@users.noreply.github.com>
2022-04-24 01:30:36 +0400

    task

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.GroupName("Programming");
+            student.GroupName("Design");
+            Student student2 = new Student();
+            Student student3 = new Student();
+            student3.GroupName("Design");
+            Console.WriteLine(student.GroupName("Design"));
+            Console.WriteLine(student.GroupName("Programming"));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Student
+    {
+        private int _id;
+        public static int Id { get; set; }  
+        public string Fullname { get; set; }    
+        public string Subject { get; set; }  
+        public Student()
+        {
+            _id = 1000;
+            _id++;
+            Id = _id;
+        }
+        public string GroupName(string Subject)
+        {
+          if(Subject == "Programming")
+            {
+                return  "PR"+ Id;
+            }
+          else if (Subject == "Design")
+            {
+                return  "DE"+ Id;
+            }
+            return "bele bir qrup tapilmaqdi";
+        }
+    } 
+}
